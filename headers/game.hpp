c2ac5f11447d0652fec209bbb1b1fb06dfb75553
Sid Shingaki <sidalaytv@gmail.com>
2022-07-05 00:43:43 -0700

    refactor: (WIP) add GAME.HPP
    - game.hpp added to refactor and organize main.cpp clutter
    - updated cmakelists to include game.cpp

---
+++ headers/game.hpp
@@ -0,0 +1,33 @@
+#ifndef GAME_HPP
+#define GAME_HPP
+
+namespace Game
+{
+    enum class State
+    {
+        START, 
+        PAUSED, 
+        RUNNING, 
+        TRANSITION, 
+        EXIT, 
+        GAMEOVER
+    };
+
+    struct Info
+    {
+        
+    };
+
+    struct Objects
+    {
+
+    };
+
+    void Run();
+    void Initialize();
+    void CheckScreenSizing();
+    void Loop();
+    void Tick();
+}
+
+#endif
