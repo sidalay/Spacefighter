6778cc32ad4ad7a7e9d2b89fbde737fcc3e54f35
Sid Shingaki <sidalaytv@gmail.com>
2022-07-06 02:57:28 -0700

    style: fixed member names to UpperCamelCase

--- include/basecharacter.hpp
+++ include/basecharacter.hpp
@@ -1,31 +1,27 @@
 #ifndef BASECHARACTER_HPP
 #define BASECHARACTER_HPP
 
-#include <raylib.h>
-
 class BaseCharacter
 {
 protected:
-    Texture2D texture{};
-
-    bool alive{true};
-    int currentFrame{};
-    int previousFrame{};
-    int maxFrames{7};
-    float updateTime{1.f/14.f};
-    float runningTime{};
-    float health {};
-    float speed {4};
-    float width {};
-    float height {};
-    float scale {4.f};
+    bool Alive{true};
+    int CurrentFrame{};
+    int PreviousFrame{};
+    int MaxFrames{7};
+    float UpdateTime{1.f/14.f};
+    float RunningTime{};
+    float Health {};
+    float Speed {4};
+    float Width {};
+    float Height {};
+    float Scale {4.f};
 
 public:
     BaseCharacter();
     // void undoMovement();
     // Rectangle getCollisionRec();
-    virtual void tick(float deltaTime);
-    float getHealth() const {return health;}
+    virtual void Tick(float deltaTime);
+    float GetHealth() const {return Health;}
 };
 
 #endif
--- include/ship.hpp
+++ include/ship.hpp
@@ -3,28 +3,19 @@
 
 // #include <vector>
 #include "basecharacter.hpp"
-#include "window.hpp"
 // #include "projectile.hpp"
 
 class Ship : BaseCharacter
 {
-private:
-    Texture2D idle {LoadTexture("sprites/ShipNorm.png")};
-    Texture2D accel {LoadTexture("sprites/ShipAccel.png")};
-    Texture2D decel {LoadTexture("sprites/ShipDecel.png")};
-    Texture2D projectile {LoadTexture("sprites/Projectile.png")};
-    float sourceY{};            // Change the Y portion of the sprite being drawn
-    float bulletWidth{};
-    float bulletHeight{};
-    Vector2 ScreenPos {Window::Dimension.x/2, Window::Dimension.y/2};
-
-    // int MaxBullets{3};
-    // std::vector<Projectile> Bullets {Projectile{ScreenPos}, Projectile{ScreenPos}, Projectile{ScreenPos}}; 
-
 public:
     Ship();
     ~Ship();
-    void tick(float DeltaTime) override;
+    void Tick(float DeltaTime) override;
+private:
+    float SourceY{};            // Change the Y portion of the sprite being drawn
+    float BulletWidth{};
+    float BulletHeight{};
+    Vector2 ScreenPos{};
 };
 
 #endif
