d9d53fb4f416ea4ba75c781309d144aac20bcea9
Sid Shingaki <sidalaytv@gmail.com>
2022-07-06 02:45:51 -0700

    chore: cleaned & formatted main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -1,25 +1,7 @@
-#include <raylib.h>
-#include <raylib-cpp.hpp>
+#include "game.hpp"
 
-int main() {
-
-    Ship Spaceship{};
-    Texture2D SpaceBG {LoadTexture("sprites/SpaceBG.png")};
-
-    while (!WindowShouldClose())
-    {
-        BeginDrawing();
-        ClearBackground(DARKGRAY);
-
-        float DeltaTime {GetFrameTime()};
-
-        Background::DrawBackground(SpaceBG);
-        Background::Tick(DeltaTime);
-        Spaceship.tick(DeltaTime);
-
-        EndDrawing();
-    }
-    
-    CloseWindow();
+int main() 
+{
+    Game::Run();
 }
 
