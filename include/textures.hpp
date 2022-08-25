e89db968119d35e4cc7bdeaf3cef0c53b7983f63
Sid Shingaki <sidalaytv@gmail.com>
2022-08-25 01:25:23 -0700

    chore: Death sprite
    - added Death sprite to textures class

--- include/textures.hpp
+++ include/textures.hpp
@@ -12,14 +12,12 @@ struct GameTexture
     GameTexture& operator=(const GameTexture&) = delete;
     GameTexture& operator=(GameTexture&&) = default;
 
-    // Alien Sprites
     const raylib::Texture Aliens{"sprites/Aliens.png"};
-    // Ship Sprites
     const raylib::Texture Accelerate{"sprites/ShipAccel.png"};
     const raylib::Texture Decelerate{"sprites/ShipDecel.png"};
+    const raylib::Texture Death{"sprites/Death.png"};
     const raylib::Texture Normal{"sprites/ShipNorm.png"};
     const raylib::Texture Projectiles{"sprites/Projectiles.png"};
-    // Background Sprites
     const raylib::Texture SpaceBackground{"sprites/SpaceBG.png"};
 };
 
