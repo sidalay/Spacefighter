66e2a5859ce1a24e8255116699e9a5983feff811
Sid Shingaki <sidalaytv@gmail.com>
2022-08-25 21:05:59 -0700

    feat: new sprites
    - add asteroid texture
    - add spacebgoverlay texture
    - add asteroid / spacebgoverlay to textures.hpp

--- include/textures.hpp
+++ include/textures.hpp
@@ -13,12 +13,14 @@ struct GameTexture
     GameTexture& operator=(GameTexture&&) = default;
 
     const raylib::Texture Aliens{"sprites/Aliens.png"};
+    const raylib::Texture Asteroid{"sprites/Asteroid.png"};
     const raylib::Texture Accelerate{"sprites/ShipAccel.png"};
     const raylib::Texture Decelerate{"sprites/ShipDecel.png"};
     const raylib::Texture Death{"sprites/Death.png"};
     const raylib::Texture Normal{"sprites/ShipNorm.png"};
     const raylib::Texture Projectiles{"sprites/Projectiles.png"};
     const raylib::Texture SpaceBackground{"sprites/SpaceBG.png"};
+    const raylib::Texture SpaceBackgroundOverlay{"sprites/SpaceBGOverlay.png"};
 };
 
 #endif // TEXTURES_HPP
